5745e6ae789db7f29a4e2c798b5e5cf54ccc7aed
yuArt <yuliya.artishevskaya@gmail.com>
2014-09-25 15:50:03 +0400

    Connecting to server with multiples clients

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,53 +1,84 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-/**
- * Created by Yuliya on 24.09.2014.
- */
 public class Server {
+    static final int PORT = 1234;
 
     public static void main(String args[]) throws IOException {
         ServerSocket server = null;
         Socket socketClient = null;
 
-        BufferedReader in = null;
-        PrintWriter out = null;
-
         System.out.println("We are in the server");
 
         try {
-            server = new ServerSocket(1234);
+            server = new ServerSocket(PORT);
         }
         catch (IOException e) {
-            System.out.println("Couldn't listen to port 1234");
+            System.out.println("Couldn't listen to port " + PORT);
             System.exit(-1);
         }
 
+        while (true) {
+            try {
+                socketClient = server.accept();
+                System.out.println("Client connected");
+            } catch (IOException e) {
+                System.out.println("Couldn't accept");
+            }
+
+            new ServerThread(socketClient).start();
+        }
+
+    }
+}
+
+class ServerThread extends Thread {
+    Socket socket = null;
+
+    public ServerThread(Socket socket) {
+        this.socket = socket;
+    }
+
+
+    public void run() {
+        BufferedReader in = null;
+        PrintWriter out = null;
+
         try {
-            socketClient = server.accept();
-            System.out.println("Client connected");
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
         }
         catch (IOException e) {
-            System.out.println("Couldn't accept");
+            e.printStackTrace();
+            return;
         }
 
-        in = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
-        out = new PrintWriter(socketClient.getOutputStream(), true);
         String input, output;
 
-        while ((input = in.readLine()) != null) {
-            if (input.equalsIgnoreCase("exit")) break;
-            out.println(">> " + input);
-            System.out.println(input);
+        try {
+            while ((input = in.readLine()) != null) {
+                if (input.equalsIgnoreCase("exit")) break;
+                out.println(">> " + input);
+                System.out.println(input);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return;
         }
 
-        in.close();
-        out.close();
-        socketClient.close();
-        server.close();
+        finally {
+            try {
+                if (in != null) in.close();
+                if (out != null) out.close();
+                if (socket != null) socket.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+
     }
 }
